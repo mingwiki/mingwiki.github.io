0fb833c69cb8efe7d56bbea3cf89f9666c46fb9d
mingwiki <mingwiki@gmail.com>
2022-11-09 14:44:06 +0800

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,37 @@ import { useState, useRef, FormEvent, useEffect } from 'react'
 import './App.scss'
 
 function App() {
-  const [value, setValue] = useState<string>('')
   const [suggestions, setSuggestions] = useState<Array<object>>([])
   const searchRef = useRef<HTMLInputElement>(null)
+  const searchValueRef = useRef<string>(null)
+  const $ = (el: string) => document.querySelector(el)
 
-  const handleKeydown = (e: KeyboardEvent, value: string): void => {
+  const handleKeydown = (e: KeyboardEvent): void => {
+    document.activeElement !== searchRef.current && searchRef.current.focus()
+    const value = encodeURIComponent(searchValueRef.current)
     let url = ''
+    if (e.key === 'Enter') {
+      url = `https://google.com/search?q=${value}&pws=0&gl=us&gws_rd=cr`
+    }
     if (e.ctrlKey && e.key === 'Enter') {
       url = `https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=${value}`
     }
     if (e.shiftKey && e.key === 'Enter') {
+      url = `https://github.com/search?q=${value}`
     }
-    if (e.key === 'Enter') {
-      url = `https://google.com/search?q=${value}&pws=0&gl=us&gws_rd=cr`
+    if (url) {
+      open(url, '_blank')
+      return
     }
-    if (e.key === 'Escape') {
-      console.log('hello Escape')
-    }
-    if (url) location.assign(url)
+    handleSuggestions()
   }
   const jsonp = (url: string) => {
-    let script = document.createElement('script')
+    const script = document.createElement('script')
     script.src = url
-    document.querySelector('head')?.appendChild(script)
-    document.querySelector('head')?.removeChild(script)
+    $('head')?.appendChild(script)
+    $('head')?.removeChild(script)
   }
-  const handleSuggestions = (val: string): void => {
+  const handleSuggestions = (): void => {
     const callback: string = 'autocompleteCallback'
     window[callback] = (res: Array<any>) => {
       setSuggestions(res[1])
@@ -36,30 +41,26 @@ function App() {
       google: 'https://www.google.com/complete/search?client=chrome&hl=en',
       duckduckgo: 'https://duckduckgo.com/ac?',
     }
-    jsonp(`${suggestEngine.google}&q=${val}&callback=${callback}`)
+    jsonp(
+      `${suggestEngine.google}&q=${searchValueRef.current}&callback=${callback}`,
+    )
   }
   useEffect(() => {
-    value && handleSuggestions(value)
-  }, [value])
-  useEffect(() => {
-    document.addEventListener('keydown', (e) => handleKeydown(e, value))
-    searchRef.current && searchRef.current.focus()
-  }, [value])
+    document.addEventListener('keydown', handleKeydown)
+  }, [])
   return (
     <div className='App'>
       <input
         ref={searchRef}
         type='text'
         aria-label='search'
-        value={value}
         onInput={(e: FormEvent<HTMLInputElement>) => {
-          setValue((e.target as HTMLInputElement).value)
+          searchValueRef.current = (e.target as HTMLInputElement).value
         }}
       />
       <div>
-        {suggestions.map((i, idx) => (
-          <div key={idx}>{i}</div>
-        ))}
+        {searchValueRef.current &&
+          suggestions.map((i, idx) => <div key={idx}>{i}</div>)}
       </div>
     </div>
   )
